94a4275a2d51b85237ae24a437676f51789b78c1
tudor.farcasanu <tudor.farcasanu@fep7-1.grid.pub.ro>
2021-04-30 20:51:18 +0300

    cerinta1 working

--- src/cerinta1.c
+++ src/cerinta1.c
@@ -8,8 +8,21 @@
  * @note   -Atentie! Nu redeschideti fisierul de intrare
  * @retval - Graful in reprezentarea matrice de adiacenta
  */
-Graph* createGraph(FILE *input){
-
+Graph *createGraph(FILE *input)
+{
+    int u = 0, v = 0;
+    Graph *g = (Graph *)malloc(sizeof(Graph));
+    fscanf(input, "%d%d", &(g->V), &(g->E));
+    g->mat = (int **)calloc(g->V, sizeof(int *));
+    for (int i = 0; i < g->V; i++)
+        g->mat[i] = (int *)calloc(g->V, sizeof(int));
+    for (int i = 0; i < g->E; i++)
+    {
+        fscanf(input, "%d%d", &(u), &(v));
+        fscanf(input,"%d",&g->mat[u][v]);
+        g->mat[v][u]=g->mat[u][v];
+    }
+    return g;
 }
 /**
  * TODO: Implementati functia de afisare a matricei de adiacenta
@@ -17,6 +30,12 @@ Graph* createGraph(FILE *input){
  * @note : Nice to have
  * @retval - None
  */
-void printMat(Graph *g){
-   
+void printMat(Graph *g)
+{
+    for (int i = 0; i < g->V; i++)
+    {
+        for (int j = 0; j < g->V; j++)
+            printf("%d ", g->mat[i][j]);
+        printf("\n");
+    }
 }

6ffdf86fe3b60f7b2c74c030cb248ed00f1203bf
tudor.farcasanu <tudor.farcasanu@fep7-1.grid.pub.ro>
2021-05-08 01:40:48 +0300

    heap halfway?

--- src/heap.c
+++ src/heap.c
@@ -4,9 +4,22 @@
  * @param  capacitate: capacitatea heap-ului
  * @retval - heap-ul creat
  */
-MinHeap* create(int capacitate) {
-
+MinHeap *create(int capacitate)
+{
+	MinHeap *h = (MinHeap *)malloc(sizeof(MinHeap));
+	h->size = 0;
+	h->capacity = capacitate;
+	h->poz = (int *)malloc(sizeof(int) * h->capacity);
+	for (int i = 0; i < h->capacity; i++)
+		h->poz[i] = -1;
+	h->arr = (HeapNode **)calloc(h->capacity, sizeof(HeapNode *));
+	for (int i = 0; i < h->capacity; i++)
+	{
+		h->arr[i] = (HeapNode *)calloc(1, sizeof(HeapNode));
+		h->arr[i]->distanta=INT_MAX;
+	}
 	
+	return h;
 }
 
 /**
@@ -15,9 +28,12 @@ MinHeap* create(int capacitate) {
  * @param dist: distanta rezultata in urma implementarii algoritmului Dijkstra
  * @retval - HeapNode-ul creat
  */
-HeapNode *createHeapNode(int v, int dist) {
-
-   
+HeapNode *createHeapNode(int v, int dist)
+{
+	HeapNode *n = (HeapNode *)calloc(1, sizeof(HeapNode)); // doar pt testat daca trebuie vectorul declarat deja maxim
+	n->v = v;
+	n->distanta = dist;
+	return n;
 }
 
 /**
@@ -28,9 +44,11 @@ HeapNode *createHeapNode(int v, int dist) {
  * @retval - parintele copilului i
  */
 
-int parent(MinHeap *heap, int i) {
-
-   
+int parent(MinHeap *heap, int i)
+{
+	if (i > heap->size - 1 || i <= 0)
+		return -1;
+	return (i - 1) / 2;
 }
 /**
  * TODO:  Implementati functia de returnare a index-ului
@@ -40,9 +58,12 @@ int parent(MinHeap *heap, int i) {
  * @retval - copilul din stanga al parintelui i
  */
 
-int leftChild(MinHeap *heap, int i) {
-
-  
+int leftChild(MinHeap *heap, int i)
+{
+	int poz = 2 * i + 1;
+	if (poz > heap->size - 1 || i < 0)
+		return -1;
+	return poz;
 }
 
 /**
@@ -52,8 +73,12 @@ int leftChild(MinHeap *heap, int i) {
  * @param  i: parintele i
  * @retval - copilul din dreapta al parintelui i
  */
-int rightChild(MinHeap *heap, int i) {
-
+int rightChild(MinHeap *heap, int i)
+{
+	int poz = 2 * i + 2;
+	if (poz > heap->size - 1 || i < 0)
+		return -1;
+	return poz;
 }
 
 /**
@@ -62,9 +87,10 @@ int rightChild(MinHeap *heap, int i) {
  * @param  heap: MinHeap-ul
  * @retval - HeapNode-ul in care este stocata radacina
  */
-HeapNode* returnRoot(MinHeap *heap) {
-
-   
+HeapNode *returnRoot(MinHeap *heap)
+{
+	if (heap->size)
+		return heap->arr[0];
 }
 
 /**
@@ -73,17 +99,49 @@ HeapNode* returnRoot(MinHeap *heap) {
  * @param  i: pozitia de pe care se doreste
  * @retval None
  */
-void heapify(MinHeap *heap, int i) {
-
+void heapify(MinHeap *heap, int i)
+{
+	int l, r, min;
+	HeapNode *aux;
+	while (1)
+	{
+		min = i;
+		l = leftChild(heap, i);
+		r = rightChild(heap, i);
+		if (l != -1 && heap->arr[l]->distanta < heap->arr[min]->distanta)
+			min = l;
+		if (r != -1 && heap->arr[r]->distanta < heap->arr[min]->distanta)
+			min = r;
+		if (min != i)
+		{
+			aux = heap->arr[i];
+			heap->arr[i] = heap->arr[min];
+			heap->arr[min] = aux;
+			i = min;
+		}
+		else
+			break;
+	}
 }
 /**
  * TODO: Implementati functia de redimensionare a MinHeap-ului  
  * @param  heap: MinHeap-ul
  * @retval None
  */
-void resize(MinHeap *heap) {
-
-	
+void resize(MinHeap *heap)
+{
+	HeapNode **v = (HeapNode **)realloc(heap->arr, heap->capacity = heap->capacity * 2);
+	int *g = (int *)realloc(heap->poz, heap->capacity = heap->capacity * 2);
+	if (v == NULL)
+		return;
+	if (heap->arr != NULL)
+		free(heap->arr);
+	heap->arr = v;
+	if (g == NULL)
+		return;
+	if (heap->poz != NULL)
+		free(heap->poz);
+	heap->poz = g;
 }
 /**
  * TODO: Implementati functia de inserare a unui nod in heap
@@ -91,22 +149,31 @@ void resize(MinHeap *heap) {
  * @param  x: HeapNode-ul care urmeaza a fi inserat
  * @retval None
  */
- 
-void insert(MinHeap *heap, HeapNode *x){
 
+void insert(MinHeap *heap, HeapNode *x)
+{
+	int i;
+	if (heap->size == heap->capacity)
+		resize(heap);
+	i = heap->size;
+	heap->size++;
+	while (i > 0 && x->distanta < heap->arr[(i - 1) / 2]->distanta)
+	{
+		heap->arr[i] = heap->arr[(i - 1) / 2];
+		heap->poz[i] = heap->poz[(i - 1) / 2];
+		i = (i - 1) / 2;
+	}
+	heap->arr[i] = x;
+	heap->poz[i] = x->v-1;
 }
-
-
-
 
 /**
  * TODO: Implementati functia stergere a unui heap
  * @param  h: MinHeap-ul
  * @retval None
  */
-void deleteMinHeap(MinHeap **heap){
-
-	
+void deleteMinHeap(MinHeap **heap)
+{
 }
 /**
  * TODO: Implementati functia de afisare a unui heap
@@ -114,7 +181,8 @@ void deleteMinHeap(MinHeap **heap){
  * @note : se va afisa si pozitia HeapNode-urilor, pe langa componentele HeapNode-ului
  * @retval None
  */
-void printHeap(MinHeap *heap){
+void printHeap(MinHeap *heap)
+{
 	//nice to have
 }
 /**
@@ -122,6 +190,6 @@ void printHeap(MinHeap *heap){
  * @param  heap: MinHeap-ul
  * @retval HeapNode-ul care s-a sters, fosta radacina
  */
-HeapNode* deleteNode(MinHeap *heap){
-
+HeapNode *deleteNode(MinHeap *heap)
+{
 }

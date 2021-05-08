72b763c88bad20db29f6c55b15031d1e7023dcf2
tudor.farcasanu <tudor.farcasanu@fep7-1.grid.pub.ro>
2021-05-08 23:48:50 +0300

    ready
    Co-authored-by: HyperLucario <HyperLucario@users.noreply.github.com>

--- src/cerinta2.c
+++ src/cerinta2.c
@@ -1,7 +1,6 @@
 #include "graph.h"
 #include "heap.h"
 
-
 /**
  * TODO:   Implementati Algoritmul Dijkstra folosindu-va de MinHeap-ul creat in fisierul heap.c
  * @param  g: graful dat
@@ -9,8 +8,52 @@
  * @retval None
  */
 
-DijkstraPath* Dijkstra(Graph *g, int source){
+DijkstraPath *Dijkstra(Graph *g, int source)
+{
+    HeapNode *u, *sursa = createHeapNode(source, 0), *aux;
+    DijkstraPath *n = (DijkstraPath *)calloc(1, sizeof(DijkstraPath));
+    n->dist = (int *)calloc(g->V, sizeof(int));
+    n->pred = (int *)calloc(g->V, sizeof(int));
+    MinHeap *h = create(g->V);
+    for (int i = 0; i < g->V; i++)
+    {
+        n->pred[i] = -1;
+        n->dist[i] = 99999;
+    }
+    n->dist[source] = 0;
+    n->pred[source] = -1;
+    insert(h, sursa);
+    // for (int i = 0; i < g->V; i++)
+    // {
+    //     for (int j = 0; j < g->V; j++)
+    //     {
+    //         if (g->mat[i][j] != 0)
+    //             aux = createHeapNode(j, g->mat[i][j]);
+    //         insert(h, aux);
+    //     }
+    // }
 
+    while (h->size)
+    {
+        // iau varful cu distanta minima pana la sursa dintre varfurile care nu sunt in sps
+        u = deleteNode(h);
+        // printf("%d\n", u->v);
+        //incep sa il procesez// Updatez valoarea distantei pentru fiecare varf adiacent cu u
+        for (int i = 0; i < g->V; i++)
+        {
+            // Updatez dist[i] daca varful i nu e in sps, daca u si i adiacente si
+            //daca distanta pe noua ruta e mai mica decat cea stocata anterior
+            if (g->mat[u->v][i] != 0 &&
+                n->dist[u->v] != 99999 && n->dist[u->v] + g->mat[u->v][i] < n->dist[i])
+            {
+                n->dist[i] = n->dist[u->v] + g->mat[u->v][i];
+                aux = createHeapNode(i, n->dist[i]);
+                insert(h, aux);
+                n->pred[i] = u->v;
+            }
+        }
+    }
+    return n;
 }
 
 /**
@@ -25,6 +68,15 @@ DijkstraPath* Dijkstra(Graph *g, int source){
  *         fisierul de output este deschis 
  * @retval None
  */
-void printPath(int *vectorPath, int index, int *vector_precedesori, int destinatie, FILE *output) {
-   
+void printPath(int *vectorPath, int index, int *vector_precedesori, int destinatie, FILE *output)
+{
+    // for (int i = 0; i < destinatie; i++)
+    // {
+        
+    // }
+    if(index==-1) return;
+    vectorPath[index]=destinatie;
+    fprintf(output,"%d ",vectorPath[index]);
+    printPath(vectorPath,index-1,vector_precedesori,vector_precedesori[destinatie],output);
+    
 }
